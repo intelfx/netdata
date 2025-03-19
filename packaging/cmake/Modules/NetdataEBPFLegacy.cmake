37b0b8d2802aeef0f8f2a82ff136b148c6b31c47
Ivan Shapovalov <intelfx@intelfx.name>
2025-03-19 20:12:58 +0700

    LOCAL: buildsystem fixes

--- packaging/cmake/Modules/NetdataEBPFLegacy.cmake
+++ packaging/cmake/Modules/NetdataEBPFLegacy.cmake
@@ -43,6 +43,6 @@ endfunction()
 
 function(netdata_install_legacy_ebpf_code)
     install(DIRECTORY ${ebpf-legacy_BUILD_DIR}/ebpf.d
-            DESTINATION usr/libexec/netdata/plugins.d
+            DESTINATION usr/lib/netdata/plugins.d
             COMPONENT ebpf-code-legacy)
 endfunction()
--- packaging/cmake/Modules/NetdataGoTools.cmake
+++ packaging/cmake/Modules/NetdataGoTools.cmake
@@ -4,11 +4,7 @@
 #
 # SPDX-License-Identifier: GPL
 
-if(CMAKE_BUILD_TYPE STREQUAL Debug)
-    set(GO_LDFLAGS "-X github.com/netdata/netdata/go/plugins/pkg/buildinfo.Version=${NETDATA_VERSION_STRING}")
-else()
-    set(GO_LDFLAGS "-w -s -X github.com/netdata/netdata/go/plugins/pkg/buildinfo.Version=${NETDATA_VERSION_STRING}")
-endif()
+set(GO_LDFLAGS "-X github.com/netdata/netdata/go/plugins/pkg/buildinfo.Version=${NETDATA_VERSION_STRING}")
 
 # add_go_target: Add a new target that needs to be built using the Go toolchain.
 #
@@ -33,7 +29,7 @@ macro(add_go_target target output build_src build_dir)
 
     add_custom_command(
         OUTPUT ${output}
-        COMMAND "${CMAKE_COMMAND}" -E env GOROOT=${GO_ROOT} CGO_ENABLED=0 GOPROXY=https://proxy.golang.org,direct "${GO_EXECUTABLE}" build -buildvcs=false -ldflags "${GO_LDFLAGS}" -o "${CMAKE_BINARY_DIR}/${output}" "./${build_dir}"
+        COMMAND "${CMAKE_COMMAND}" -E env "${GO_EXECUTABLE}" build -buildvcs=false -ldflags "${GO_LDFLAGS}" -o "${CMAKE_BINARY_DIR}/${output}" "./${build_dir}"
         DEPENDS ${${target}_DEPS}
         COMMENT "Building Go component ${output}"
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/${build_src}"
